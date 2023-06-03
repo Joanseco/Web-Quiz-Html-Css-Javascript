e451c5f864ea0430d1977514d1897a240ae53895
JoanSc <joanseco5@gmail.com>
2023-06-02 21:21:01 -0500

    Quinto commit (quiz funcional)

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const main = document.querySelector('.main');
 const continueBtn = document.querySelector('.continue-btn');
 const quizSection = document.querySelector('.quiz-section');
 const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const tryAgain = document.querySelector('.tryAgain-btn');
+const goHomeBtn = document.querySelector('.goHome-btn');
 
 startBtn.onclick = () =>{
     popupInfo.classList.add('active');
@@ -24,23 +27,55 @@ continueBtn.onclick = () =>{
 
     showQuestions(0);
     questionCounter(1);
+    headerScore();
 }
+
+tryAgain.onclick = () =>{
+    quizBox.classList.add('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+
+    headerScore();
+}
+
+goHomeBtn.onclick = ()=>{
+    quizSection.classList.remove('active');
+    nextBtn.classList.remove('active');
+    resultBox.classList.remove('active');
+
+    questionCount = 0;
+    questionNumb = 1;
+    userScore = 0;
+    showQuestions(questionCount);
+    questionCounter(questionNumb);
+}
+
+
 
 let questionCount = 0;
 let questionNumb = 1;
+let userScore = 0;
 
 const nextBtn = document.querySelector('.next-btn');
 
 nextBtn.onclick = () =>{
-    if(questionCount <questions.length -1){
+    if(questionCount <questions.length - 1){
         questionCount++;
         showQuestions(questionCount);
 
         questionNumb++;
         questionCounter(questionNumb);
+
+        nextBtn.classList.remove('active');
     }
     else{
-        console.log("Questions completed");
+        showResultBox();
     }
 }
 
@@ -67,15 +102,63 @@ function showQuestions(index){
 function optionSelected(answer){
     let userAnswer = answer.textContent.trim();
     let correctAnswer = questions[questionCount].answer.trim();
+    let allOptions = optionList.children.length;
+
     if(userAnswer == correctAnswer){
         answer.classList.add('correct');
+        userScore += 1;
+        headerScore();
     }
     else{
         answer.classList.add('incorrect');
+        
+        for (let i = 0; i < allOptions; i++){
+            if (optionList.children[i].textContent.trim() == correctAnswer) {
+                optionList.children[i].setAttribute('class', 'option correct'); 
+            }
+        }        
     }
+
+    //si el usuario seleciona una opcion las demas se bloquean
+    for(let i = 0; i < allOptions ; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+
+    nextBtn.classList.add('active');
 }
 
 function questionCounter(index){
     const questionTotal = document.querySelector('.question-total');
     questionTotal.textContent = `${index} of ${questions.length} Questions`;
+}
+
+function headerScore() {
+    const headerScoreText = document.querySelector('.header-score');
+    headerScoreText.textContent = `Puntaje: ${userScore} / ${questions.length}`;
+}
+
+function showResultBox(){
+    quizBox.classList.remove('active');
+    resultBox.classList.add('active');
+
+    const scoreText = document.querySelector('.score-text');
+    scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
+
+    const circularProgress = document.querySelector('.circular-progress');
+    const progressValue = document.querySelector('.progress-value');
+
+    let progressStartValue = -1;
+    let progressEndValue = (userScore / questions.length) * 100;
+    let speed = 20;
+
+    let progress = setInterval(() =>{
+        progressStartValue++;
+
+        progressValue.textContent = `${progressStartValue}%`;
+        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+
+        if(progressStartValue==progressEndValue){
+            clearInterval(progress);
+        }
+    }, speed);
 }
